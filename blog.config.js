56a107a00ac8bea0e1baff1bb55e569bc65d395e
zmendax <2201382458@qq.com>
2022-10-24 13:56:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -33,10 +33,10 @@ const BLOG = {
 
   POST_LIST_STYLE: 'page', // ['page','scroll] 文章列表样式:页码分页、单页滚动加载
   POST_LIST_PREVIEW: process.env.NEXT_PUBLIC_POST_PREVIEW || 'true', //  是否在列表加载文章预览
-  POST_PREVIEW_LINES: 4, // 预览博客行数
-  POST_RECOMMEND_COUNT: 6, // 推荐文章数量
-  POSTS_PER_PAGE: 6, // post counts per page
-  POSTS_SORT_BY: 'notion', // 排序方式 'date'按时间,'notion'由notion控制
+  POST_PREVIEW_LINES: 3, // 预览博客行数
+  POST_RECOMMEND_COUNT: 10, // 推荐文章数量
+  POSTS_PER_PAGE: 10, // post counts per page
+  POSTS_SORT_BY: 'date', // 排序方式 'date'按时间,'notion'由notion控制
 
   PREVIEW_CATEGORY_COUNT: 16, // 首页最多展示的分类数量，0为不限制
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
